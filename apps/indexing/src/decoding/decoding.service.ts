a7ba900cf3e42a3ed2b769f3ac7051d3d8b4b594
samuel-videau <samuel.videau@efrei.net>
2023-03-30 12:33:53 +0700

    test: test suite for decoding service

--- apps/indexing/src/decoding/decoding.service.ts
+++ apps/indexing/src/decoding/decoding.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import Web3 from 'web3';
 
-import { TxParameterTable } from '../../../../libs/database/lukso-data/entities/txParameter.table';
 import { LuksoStructureDbService } from '../../../../libs/database/lukso-structure/lukso-structure-db.service';
+
+interface DecodedParameter {
+  value: string;
+  position: number;
+  name: string;
+  type: string;
+}
 
 @Injectable()
 export class DecodingService {
@@ -12,28 +18,35 @@ export class DecodingService {
     this.web3 = new Web3();
   }
 
+  /**
+   * Identifies and decodes the transaction input data.
+   *
+   * @param {string} input - The input data string of the transaction.
+   * @returns {Promise<{parameters: DecodedParameter[]; methodName: string;} | null>} - An object containing the decoded
+   * parameters and the method name, or null if the method interface is not found.
+   */
   public async identifyAndDecodeTransactionInput(input: string): Promise<{
-    parameters: Omit<TxParameterTable, 'transactionHash' | 'unwrapped'>[];
+    parameters: DecodedParameter[];
     methodName: string;
   } | null> {
     const methodId = input.slice(0, 10);
-    const parameters: Omit<TxParameterTable, 'transactionHash' | 'unwrapped'>[] = [];
+
     const methodInterface = await this.structureDB.getMethodInterfaceById(methodId);
     if (!methodInterface) return null;
 
     const methodName = methodInterface.name;
-
     const methodParameters = await this.structureDB.getMethodParametersByMethodId(methodId);
+
+    // Decode the parameters using the Web3 library.
     const decodedParameters = this.web3.eth.abi.decodeParameters(methodParameters, input.slice(10));
 
-    methodParameters.forEach((parameter) => {
-      parameters.push({
-        value: decodedParameters[parameter.name] as string,
-        position: parameter.position,
-        name: parameter.name,
-        type: parameter.type,
-      });
-    });
+    // Map the decoded parameters to the DecodedParameter[] format.
+    const parameters: DecodedParameter[] = methodParameters.map((parameter) => ({
+      value: decodedParameters[parameter.name] as string,
+      position: parameter.position,
+      name: parameter.name,
+      type: parameter.type,
+    }));
 
     return { methodName, parameters };
   }
---
+++ apps/indexing/src/decoding/decoding.spec.ts
@@ -0,0 +1,155 @@
+import { Test } from '@nestjs/testing';
+
+import { DecodingService } from './decoding.service';
+import { LuksoStructureDbService } from '../../../../libs/database/lukso-structure/lukso-structure-db.service';
+import { MethodInterfaceTable } from '../../../../libs/database/lukso-structure/entities/methodInterface.table';
+import { MethodParameterTable } from '../../../../libs/database/lukso-structure/entities/methodParameter.table';
+
+describe('DecodingService', () => {
+  let service: DecodingService;
+  const db = new LuksoStructureDbService();
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [DecodingService, { provide: LuksoStructureDbService, useValue: db }],
+    }).compile();
+
+    service = moduleRef.get<DecodingService>(DecodingService);
+  });
+
+  describe('identifyAndDecodeTransactionInput', () => {
+    it('should return null if method ID is not found in structure database', async () => {
+      const input = '0xdeadbeef';
+      const result = await service.identifyAndDecodeTransactionInput(input);
+      expect(result).toBeNull();
+    });
+
+    it('should be able to decode a setData input', async () => {
+      const methodInterface: MethodInterfaceTable = {
+        id: '0x7f23690c',
+        hash: '0x7f23690cc80a596acdae0a51503f8066ffd79ab76fd5c929578309d2e2883b77',
+        name: 'setData',
+        type: 'function',
+      };
+      await db.insertMethodInterface(methodInterface);
+      const parameters: MethodParameterTable[] = [
+        {
+          methodId: '0x7f23690c',
+          name: 'dataKey',
+          type: 'bytes32',
+          indexed: false,
+          position: 0,
+        },
+        {
+          methodId: '0x7f23690c',
+          name: 'dataValue',
+          type: 'bytes',
+          indexed: false,
+          position: 1,
+        },
+      ];
+      await db.insertMethodParameter(parameters[0]);
+      await db.insertMethodParameter(parameters[1]);
+
+      const inputToDecode =
+        '0x7f23690c4b80742de2bf82acb3630000254bfe7e25184f72df435b5a9da39db6089dcaf5000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000000';
+
+      const result = await service.identifyAndDecodeTransactionInput(inputToDecode);
+
+      expect(result?.methodName).toEqual('setData');
+      expect(result?.parameters).toEqual([
+        {
+          value: '0x4b80742de2bf82acb3630000254bfe7e25184f72df435b5a9da39db6089dcaf5',
+          position: 0,
+          name: 'dataKey',
+          type: 'bytes32',
+        },
+        {
+          value: '0x0000000000000000000000000000000000000000000000000000000000000000',
+          position: 1,
+          name: 'dataValue',
+          type: 'bytes',
+        },
+      ]);
+    });
+
+    it('should be able to decode an execute input', async () => {
+      const methodInterface: MethodInterfaceTable = {
+        id: '0x44c028fe',
+        hash: '0x44c028fee1c693348483835b83daaff8050a631d70676515d1e283a5b884b4aa',
+        name: 'execute',
+        type: 'function',
+      };
+      await db.insertMethodInterface(methodInterface);
+      const parameters: MethodParameterTable[] = [
+        {
+          methodId: '0x44c028fe',
+          name: 'operation',
+          type: 'uint256',
+          indexed: false,
+          position: 0,
+        },
+        {
+          methodId: '0x44c028fe',
+          name: 'to',
+          type: 'address',
+          indexed: false,
+          position: 1,
+        },
+        {
+          methodId: '0x44c028fe',
+          name: 'value',
+          type: 'uint256',
+          indexed: false,
+          position: 2,
+        },
+        {
+          methodId: '0x44c028fe',
+          name: 'data',
+          type: 'bytes',
+          indexed: false,
+          position: 3,
+        },
+      ];
+      await db.insertMethodParameter(parameters[0]);
+      await db.insertMethodParameter(parameters[1]);
+      await db.insertMethodParameter(parameters[2]);
+      await db.insertMethodParameter(parameters[3]);
+
+      const inputToDecode =
+        '0x44c028fe000000000000000000000000000000000000000000000000000000000000000000000000000000000000000036ec763516259d4be9ede7cc2969969f201139dd0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000402b4ece40c19103d32cf026a68c4c129dbda957a7337e5bb24034db0b7d4f8ee2000000000000000000000000d692ba892a902810a2ee3fa41c1d8dcd652d47ab';
+
+      const result = await service.identifyAndDecodeTransactionInput(inputToDecode);
+
+      console.log(result);
+      expect(result?.methodName).toEqual('execute');
+      expect(result?.parameters).toEqual([
+        {
+          value: '0',
+          position: 0,
+          name: 'operation',
+          type: 'uint256',
+        },
+        {
+          value: '0x36eC763516259D4bE9EDe7cC2969969f201139dd',
+          position: 1,
+          name: 'to',
+          type: 'address',
+        },
+        {
+          value: '0',
+          position: 2,
+          name: 'value',
+          type: 'uint256',
+        },
+        {
+          value:
+            '0x2b4ece40c19103d32cf026a68c4c129dbda957a7337e5bb24034db0b7d4f8ee2000000000000000000000000d692ba892a902810a2ee3fa41c1d8dcd652d47ab',
+          position: 3,
+          name: 'data',
+          type: 'bytes',
+        },
+      ]);
+    });
+  });
+});
