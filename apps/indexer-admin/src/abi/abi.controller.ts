0db95d761b2d17ddccac35d7aa71ded65cb27937
jirios125 <jir.main.business@gmail.com>
2023-09-27 01:29:43 -0300

    fix: params of controller and service

--- apps/indexer-admin/src/abi/abi.controller.ts
+++ apps/indexer-admin/src/abi/abi.controller.ts
@@ -1,7 +1,8 @@
-import {Body, Controller, HttpException, HttpStatus, Post} from '@nestjs/common';
-import { AbiService } from './abi.service';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { AbiItem } from 'web3-utils';
 import { ApiTags } from '@nestjs/swagger';
+
+import { AbiService } from './abi.service';
 
 @ApiTags('ABIs')
 @Controller('abi')
@@ -9,14 +10,11 @@ export class AbiController {
   constructor(private readonly abiService: AbiService) {}
 
   @Post()
-  async processAndUploadAbiItems(@Body() abiItems: Array<Array<AbiItem>>) {
+  async processAndUploadAbiItems(@Body() abiItems: Array<AbiItem>) {
     try {
       await this.abiService.processAndUploadAbiItems(abiItems);
     } catch {
-      throw new HttpException(
-        'Failed to process ABI items',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      throw new HttpException('Failed to process ABI items', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
--- apps/indexer-admin/src/abi/abi.service.ts
+++ apps/indexer-admin/src/abi/abi.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { AbiItem } from 'web3-utils';
 import { generateAndPersistMethodInterfaces } from '@db/lukso-structure/utils/generate-method-interfaces';
-import { LuksoStructureDbService } from '@db/lukso-structure/lukso-structure-db.service';
-import { LoggerService } from '@libs/logger/logger.service';
 
 @Injectable()
 export class AbiService {
-  async processAndUploadAbiItems(abiItems: AbiItem[][]): Promise<void> {
+  async processAndUploadAbiItems(abiItems: AbiItem[]): Promise<void> {
     try {
-      await generateAndPersistMethodInterfaces(abiItems);
+      await generateAndPersistMethodInterfaces([abiItems]);
     } catch (error) {
       throw new Error('Failed to process and upload ABI items: ' + (error as Error).message);
     }
