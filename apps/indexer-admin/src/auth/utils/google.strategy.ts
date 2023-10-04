ede901b86caac9843139b7856a5490583bcbf2ef
jirios125 <jir.main.business@gmail.com>
2023-10-04 01:45:35 -0300

    fix: strategy fixed, google singin working

--- apps/indexer-admin/src/auth/utils/google.strategy.ts
+++ apps/indexer-admin/src/auth/utils/google.strategy.ts
@@ -9,8 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callBackURL: 'http://localhost:3000/auth/google/callback',
-      redirect_uri: 'http://localhost:3000/auth/google/callback',
+      callbackURL: 'http://localhost:3000/auth/google/callback',
       scope: ['email', 'profile'],
     });
   }
