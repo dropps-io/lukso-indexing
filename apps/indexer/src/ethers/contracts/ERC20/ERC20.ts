7bfce6fa24bdd18de0aa781e77074c7628943097
jirios125 <jir.main.business@gmail.com>
2023-10-25 18:35:07 -0300

    fix: fetching data correctly

--- apps/indexer/src/ethers/contracts/ERC20/ERC20.ts
+++ apps/indexer/src/ethers/contracts/ERC20/ERC20.ts
@@ -3,11 +3,11 @@ import { ethers } from 'ethers';
 import { DebugLogger } from '@decorators/debug-logging.decorator';
 import { ExceptionHandler } from '@decorators/exception-handler.decorator';
 import { MetadataResponse } from '@shared/types/metadata-response';
-import ERC725Y_artifact from '@lukso/lsp-smart-contracts/artifacts/LSP0ERC725Account.json';
 
 import ERC20Abi from '../../utils/abis/ERC20Abi.json';
 import { EthersService } from '../../ethers.service';
 import { FetcherService } from '../../../fetcher/fetcher.service';
+
 export class ERC20 {
   constructor(
     protected readonly ethersService: EthersService,
@@ -27,13 +27,12 @@ export class ERC20 {
   @ExceptionHandler(false, true, null)
   public async fetchData(address: string): Promise<MetadataResponse | null> {
     try {
-      const erc20Data = await this.getERC20Contract;
       return {
         metadata: {
           address,
           tokenId: null,
-          name: erc20Data?.name || null,
-          symbol: null,
+          name: (await this.getName(address)) || null,
+          symbol: (await this.getSymbol(address)) || null,
           description: null,
           isNFT: null,
         },
@@ -50,20 +49,44 @@ export class ERC20 {
     }
   }
 
-  public async balanceOf(address: string, holderAddress: string): Promise<string> {
-    const erc20contract = this.getERC20Contract(address);
-    const balanceOf = await erc20contract.balanceOf(holderAddress);
-    return balanceOf.toString();
+  public async balanceOf(address: string, holderAddress: string): Promise<string | null> {
+    try {
+      const erc20contract = this.getERC20Contract(address);
+      const balanceOf = await erc20contract.balanceOf(holderAddress);
+      return balanceOf.toString();
+    } catch (e: any) {
+      this.logger.error(`Error while fetching ERC20 balance of ${address}: ${e.message}`, {
+        address,
+      });
+      return null;
+    }
+  }
+
+  public async getName(address: string): Promise<string | null> {
+    try {
+      const erc20contract = this.getERC20Contract(address);
+      return await erc20contract.name();
+    } catch (e: any) {
+      this.logger.error(`Error while fetching ERC20 name for ${address}: ${e.message}`, {
+        address,
+      });
+      return null;
+    }
+  }
+
+  public async getSymbol(address: string): Promise<string | null> {
+    try {
+      const erc20contract = this.getERC20Contract(address);
+      return await erc20contract.symbol();
+    } catch (e: any) {
+      this.logger.error(`Error while fetching ERC20 symbol for ${address}: ${e.message}`, {
+        address,
+      });
+      return null;
+    }
   }
 
   private getERC20Contract(address: string) {
     return new ethers.Contract(address, ERC20Abi, this.ethersService.getProvider());
-  }
-
-  private async getERC20Metadata(address: string, dataKey: string): Promise<string | null> {
-    const contract = new ethers.Contract(address, ERC20Abi, this.ethersService.getProvider());
-    const response = await contract.getData(dataKey);
-    if (!response || response === '0x') return null;
-    else return response;
   }
 }
