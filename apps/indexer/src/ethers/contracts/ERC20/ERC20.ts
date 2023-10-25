f6c2292812b072f26cbcd3dea95469fdd26e8cb7
jirios125 <jir.main.business@gmail.com>
2023-10-25 10:26:13 -0300

    wip: fetchData

--- apps/indexer/src/ethers/contracts/ERC20/ERC20.ts
+++ apps/indexer/src/ethers/contracts/ERC20/ERC20.ts
@@ -3,20 +3,17 @@ import { ethers } from 'ethers';
 import { DebugLogger } from '@decorators/debug-logging.decorator';
 import { ExceptionHandler } from '@decorators/exception-handler.decorator';
 import { MetadataResponse } from '@shared/types/metadata-response';
-import { ERC20MetadataResponse } from '@shared/types/erc20-metadata-response';
+import ERC725Y_artifact from '@lukso/lsp-smart-contracts/artifacts/LSP0ERC725Account.json';
 
 import ERC20Abi from '../../utils/abis/ERC20Abi.json';
 import { EthersService } from '../../ethers.service';
 import { FetcherService } from '../../../fetcher/fetcher.service';
 export class ERC20 {
-  private readonly erc20: ERC20;
   constructor(
     protected readonly ethersService: EthersService,
     protected readonly fetcherService: FetcherService,
     protected readonly logger: winston.Logger,
-  ) {
-    this.erc20 = new ERC20(ethersService, fetcherService, this.logger);
-  }
+  ) {}
 
   /**
    * Decodes an ERC20 token ID based on its type.
@@ -28,17 +25,22 @@ export class ERC20 {
 
   @DebugLogger()
   @ExceptionHandler(false, true, null)
-  public async fetchData(address: string): Promise<ERC20MetadataResponse | null> {
+  public async fetchData(address: string): Promise<MetadataResponse | null> {
     try {
-      const erc20Data = await this.erc20.fetchData(address);
+      const erc20Data = await this.getERC20Contract;
       return {
         metadata: {
           address,
           tokenId: null,
-          name: erc20Data?.metadata.name || null,
-          symbol: erc20Data?.metadata.symbol || null,
-          description: erc20Data?.metadata.description || null,
+          name: erc20Data?.name || null,
+          symbol: null,
+          description: null,
+          isNFT: null,
         },
+        assets: [],
+        tags: [],
+        images: [],
+        links: [],
       };
     } catch (e: any) {
       this.logger.error(`Error while fetching ERC20 data for ${address}: ${e.message}`, {
@@ -56,5 +58,12 @@ export class ERC20 {
 
   private getERC20Contract(address: string) {
     return new ethers.Contract(address, ERC20Abi, this.ethersService.getProvider());
+  }
+
+  private async getERC20Metadata(address: string, dataKey: string): Promise<string | null> {
+    const contract = new ethers.Contract(address, ERC20Abi, this.ethersService.getProvider());
+    const response = await contract.getData(dataKey);
+    if (!response || response === '0x') return null;
+    else return response;
   }
 }
--- shared/types/erc20-metadata-response.ts
+++
@@ -1,5 +0,0 @@
-import { MetadataTable } from '@db/lukso-data/entities/metadata.table';
-
-export interface ERC20MetadataResponse {
-  metadata: Omit<MetadataTable, 'id' | 'isNFT'>;
-}
