6e446f7d7e359a19d1f52a0f8445e459a0628d62
Juan Ignacio Rios <56406317+jirios125@users.noreply.github.com>
2023-11-02 09:49:58 -0300

    fix: PR comments

--- apps/indexer/src/ethers/contracts/ERC20/ERC20.ts
+++ apps/indexer/src/ethers/contracts/ERC20/ERC20.ts
@@ -24,64 +24,40 @@ export class ERC20 {
   @DebugLogger()
   @ExceptionHandler(false, true, null)
   public async fetchData(address: string): Promise<MetadataResponse | null> {
-    try {
-      return {
-        metadata: {
-          address,
-          tokenId: null,
-          name: (await this.getName(address)) || null,
-          symbol: (await this.getSymbol(address)) || null,
-          description: null,
-          isNFT: null,
-        },
-        assets: [],
-        tags: [],
-        images: [],
-        links: [],
-      };
-    } catch (e: any) {
-      this.logger.error(`Error while fetching ERC20 data for ${address}: ${e.message}`, {
+    return {
+      metadata: {
         address,
-      });
-      return null;
-    }
+        tokenId: null,
+        name: (await this.getName(address)) || null,
+        symbol: (await this.getSymbol(address)) || null,
+        description: null,
+        isNFT: null,
+      },
+      assets: [],
+      tags: [],
+      images: [],
+      links: [],
+    };
   }
 
+  @DebugLogger()
+  @ExceptionHandler(false, true, null)
   public async balanceOf(address: string, holderAddress: string): Promise<string | null> {
-    try {
-      const erc20contract = this.getERC20Contract(address);
-      const balanceOf = await erc20contract.balanceOf(holderAddress);
-      return balanceOf.toString();
-    } catch (e: any) {
-      this.logger.error(`Error while fetching ERC20 balance of ${address}: ${e.message}`, {
-        address,
-      });
-      return null;
-    }
+    const erc20contract = this.getERC20Contract(address);
+    return await erc20contract.balanceOf(holderAddress);
   }
 
+  @DebugLogger()
+  @ExceptionHandler(false, true, null)
   public async getName(address: string): Promise<string | null> {
-    try {
-      const erc20contract = this.getERC20Contract(address);
-      return await erc20contract.name();
-    } catch (e: any) {
-      this.logger.error(`Error while fetching ERC20 name for ${address}: ${e.message}`, {
-        address,
-      });
-      return null;
-    }
+    const erc20contract = this.getERC20Contract(address);
+    return await erc20contract.name();
   }
-
+  @DebugLogger()
+  @ExceptionHandler(false, true, null)
   public async getSymbol(address: string): Promise<string | null> {
-    try {
-      const erc20contract = this.getERC20Contract(address);
-      return await erc20contract.symbol();
-    } catch (e: any) {
-      this.logger.error(`Error while fetching ERC20 symbol for ${address}: ${e.message}`, {
-        address,
-      });
-      return null;
-    }
+    const erc20contract = this.getERC20Contract(address);
+    return await erc20contract.symbol();
   }
 
   private getERC20Contract(address: string) {
