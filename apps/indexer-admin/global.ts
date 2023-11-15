7af8af552013e998ea4810279da2bf22f7284463
Juan Ignacio Rios <56406317+jirios125@users.noreply.github.com>
2023-11-15 18:54:39 +0300

    fix: add google callback url to .env and mac command to indexer (#34)

--- apps/indexer-admin/global.ts
+++ apps/indexer-admin/global.ts
@@ -3,4 +3,4 @@ import process from 'process';
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 export const OAuthGoogleAPI = 'https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=';
-export const GoogleCallBackURL = 'http://localhost:3000/auth/google/callback';
+export const GoogleCallBackURL = process.env.GOOGLE_CALLBACK_URL;
