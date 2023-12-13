2b50b7b534995ac08f3ef84dbe105b036dd4566e
Juan Ignacio Rios <56406317+jirios125@users.noreply.github.com>
2023-12-13 08:45:45 -0300

    feat: add multiple run commands in cucumber

--- cucumber.js
+++ cucumber.js
@@ -1,11 +1,16 @@
-let common = [
+let indexerAdmin = [
   'apps/indexer-admin/test/features/**/*.feature', // Specify our feature files
   '--require-module ts-node/register', // Load TypeScript module
   '--require-module tsconfig-paths/register', // Load TypeScript module
   '--require apps/indexer-admin/test/step-definitions/**/*.ts', // Load step definitions
   '--format progress-bar', // Load custom formatter
-  '--format @cucumber/pretty-formatter',
 ].join(' ');
 
-module.exports = { default: common };
-console.log('Cucumber command:', common);
+let indexer = [
+  'apps/indexer/test/features/**/*.feature', // Specify our feature files
+  '--require-module ts-node/register', // Load TypeScript module
+  '--require apps/indexer/test/step-definitions/**/*.ts', // Load step definitions
+  '--format progress-bar', // Load custom formatter
+].join(' ');
+
+module.exports = { default: indexer, admin: indexerAdmin };
