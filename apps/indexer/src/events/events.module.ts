5560a9c9cf608ce828bbb41182f8eebe9f868ba3
jirios125 <jir.main.business@gmail.com>
2023-12-20 11:39:39 -0300

    fix: add redisService as provider in lsp8 and fix some imports

--- apps/indexer/src/events/events.module.ts
+++ apps/indexer/src/events/events.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from '@libs/logger/logger.module';
 import { LuksoDataDbModule } from '@db/lukso-data/lukso-data-db.module';
 import { LuksoStructureDbModule } from '@db/lukso-structure/lukso-structure-db.module';
+import { RedisModule } from '@shared/redis/redis.module';
 
 import { EventsService } from './events.service';
 import { Lsp8standardModule } from '../standards/lsp8/lsp8standard.module';
@@ -11,7 +12,6 @@ import { IndexingWsModule } from '../indexing-ws/indexing-ws.module';
 import { EthersModule } from '../ethers/ethers.module';
 import { Erc725StandardModule } from '../standards/erc725/erc725-standard.module';
 import { Erc20standardModule } from '../standards/erc20/erc20standard.module';
-import { RedisModule } from '../../../../shared/redis/redis.module';
 
 @Module({
   imports: [
--- apps/indexer/src/indexer.module.ts
+++ apps/indexer/src/indexer.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from '@libs/logger/logger.module';
+import { RedisModule } from '@shared/redis/redis.module';
 
 import { IndexerService } from './indexer.service';
 import { IndexingWsGateway } from './indexing-ws/indexing-ws.gateway';
 import { SchedulingModule } from './scheduling/scheduling.module';
 import { IndexingWsModule } from './indexing-ws/indexing-ws.module';
-import { RedisModule } from '../../../shared/redis/redis.module';
 
 @Module({
   imports: [SchedulingModule, IndexingWsModule, LoggerModule, RedisModule],
